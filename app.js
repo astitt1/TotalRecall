c2053ac041da54a49163729a059084fc38fba356
boubacarsow <42849861+boubacarsow@users.noreply.github.com>
2022-10-20 16:57:20 -0600

    updates

--- app.js
+++ app.js
@@ -1,28 +1,137 @@
-// STRINGS 
+// // STRINGS 
+// var firstVariable = "Hello World";
+// var firstVariable = 2;
+// var secondVariable;
+// secondVariable=firstVariable;
+// var secondVariable = "Hi there!"
+// console.log(secondVariable);
+
+// var yourName
+// var yourName = "boubacar";
+// console.log("Hello, my name is " + yourName);
+
+// //the value of firstVariable = 2
+
+// //BOOLEANS
+
+// const a = 4; const b = 53; const c = 57; const d = 16; const e = 'Kevin';
+
+// console.log(a < b); console.log(c > d); console.log('Name'.length < 'Kevin'.length);
 
 
+// // FOR THE NEXT TWO, USE ONLY && OR ||
 
-//BOOLEANS
+// /*note: a < b < c is NOT CORRECT 
+// (and is not a valid JS expression, 
+// think about using other math operations) 
+// console.log(a __ a ___ d); // note: 
+// the answer is a simple arithmetic equation, 
+// not something "weird"
+// */
 
-//the Farm
+// console.log(true && false); 
+// console.log(false && false && false && false && false && true); 
+// console.log(false && false);
+// console.log(e > 'Kevin'); 
+// console.log(a + b < c);
+// console.log(48 > '48');
 
-//Driver's Ed
 
+// //the Farm
 
+// const animal = "cow";
 
-//LOOPS
+// if (animal) {
+//     //do this
+//     console.log("mooooo");
+//   } else {
+//     //do this
+//     console.log("Hey! You're not a cow");
+//   };
+
+//   //Driver's Ed
+
+//   const age = 45;
+
+//   if (age >= 16) {
+//     console.log("Here are the keys!");
+//   } else {
+//       console.log("Sorry, you're too young.");
+//     }
+
+// //LOOPS
+// for(let i = 0; i < 100; i++); // This is GOOD
+// for(i = 0; i < 100; i++); // this is NO GOOD
 
 //The Basics
 
+// for (let i = 0; i <= 10; i++) {
+//   console.log(i);
+// }
+
+// for (let i = 10; i <= 400; i++) {
+//   console.log(i);
+// }
+
+// for (let i=12; i<4000; i++) {
+//   if (i % 3 == 0) {
+//     console.log(i);
+//   }
+// }
+
 //Get Even
 
+// for (let i=1; i<100; i++) {
+//     if (i % 2 == 0) {
+//       console.log(i + " is an even number");
+//     }
+//     else {console.log(i);
+//     }
+//   }
+
 //Give Me 5
+
+// for (let i=1; i<=100; i++) {
+//     if (i % 5 == 0) {
+//       console.log("I found a " + i + ". High five!");
+
+//     } if (i % 3 == 0) {
+//       console.log("I found a " + i + ". Three is a crowd"); 
+    
+//     } else {console.log(i);
+
+//     }
+
+//   }
 
 //Savings Account
 
 
+// var bank_account = 0;
+//     for(i = 0; i <= 10; i++){
+//       bank_account += i; 
+// }
+
+// console.log(bank_account);
+
+// var bank_account = 0;
+//     for(i = 1; i <= 100; i++){
+//       bank_account += i*2; 
+// }
+
+// console.log(bank_account);
+
 
 //ARRAYS & CONTROL FLOW
+//the things in array are called elements
+//Unlike objects, their items are considered to be ordered
+//e.g., let colors = ['red', 'green', 'blue'];
+
+//const quotes = ['ATLA', 'Everything Everywhere', 'Hunter x Hunter']
+
+const randomThings = [1, 10, "Hello", true];
+firstrandomThings = randomThings[0];
 
 
 
+console.log(firstrandomThings);
